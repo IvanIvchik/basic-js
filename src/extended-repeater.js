6a33cee456aef52505d7500ff69ea4fca9a5929a
IvanIvchik <ivanivcik11@gmaol.com>
2022-05-01 15:52:04 +0300

    add repeater'

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+  let separator = options.separator ? options.separator : '+';
+  let addition;
+  let additionRepeatTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+  let additionSeparator = options.additionSeparator ? options.additionSeparator : '|';
+  let res = '';
+  let strSeparator = '';
+  let newSubstr = '';
+
+  if (typeof options.addition === 'boolean') {
+    addition = options.addition ? 'true' : 'false';
+  } else if (options.addition === null) {
+    addition = 'null';
+  } else if (!options.addition) {
+    addition = '';
+  } else addition = String(options.addition);
+
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    if (i < additionRepeatTimes - 1) {
+      strSeparator += `${addition}${additionSeparator}`;
+    } else strSeparator += addition;
+  }
+  console.log(strSeparator)
+  newSubstr += String(str) + strSeparator;
+
+  for (let i = 0; i < repeatTimes; i++) {
+    if (i < repeatTimes - 1) {
+      res += newSubstr + separator;
+    } else res += newSubstr;
+  }
+  return res;
 }
 
 module.exports = {
